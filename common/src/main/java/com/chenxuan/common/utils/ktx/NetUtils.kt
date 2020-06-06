e97ea5a5bbce09a50904d2023abd38f99f1fcd04
chenxuan <18671382351@163.com>
2020-06-06 10:22:42 +0800

    add NetUtils

---
+++ common/src/main/java/com/chenxuan/common/utils/ktx/NetUtils.kt
@@ -0,0 +1,13 @@
+package com.chenxuan.common.utils.ktx
+
+import com.chenxuan.common.base.IView
+import io.reactivex.Observable
+
+fun <T> Observable<T>.async(view: IView): Observable<T> =
+    this.compose(view.bindLoading())
+        .compose(view.bindThread())
+        .compose(view.bindLifeCycle())
+
+fun <T> Observable<T>.asyncWithoutLoading(view: IView): Observable<T> =
+    this.compose(view.bindThread())
+        .compose(view.bindLifeCycle())
--- login/src/main/java/com/chenxuan/login/presenter/LoginPresenter.kt
+++ login/src/main/java/com/chenxuan/login/presenter/LoginPresenter.kt
@@ -3,6 +3,7 @@ package com.chenxuan.login.presenter
 import com.chenxuan.bean.login.ChaptersBean
 import com.chenxuan.common.base.BasePresenter
 import com.chenxuan.common.base.IView
+import com.chenxuan.common.utils.ktx.async
 import com.chenxuan.login.contract.ILogin
 import com.chenxuan.login.module.LoginModule
 import com.chenxuan.net.DefaultObserver
@@ -13,15 +14,14 @@ class LoginPresenter(view: IView) : BasePresenter<ILogin.View>(view) {
     }
 
     fun getChapters() {
-        loginModule.getChapters()
-            .compose(mViewRef?.get()?.bindLoading())
-            .compose(mViewRef?.get()?.bindThread())
-            .compose(mViewRef?.get()?.bindLifeCycle())
-            .subscribe(object : DefaultObserver<List<ChaptersBean>>() {
-                override fun onSuccess(response: List<ChaptersBean>) {
-                    mViewRef?.get()?.onSuccess(response)
-                }
-            })
+        mViewRef?.get()?.let {
+            loginModule.getChapters()
+                .async(it)
+                .subscribe(object : DefaultObserver<List<ChaptersBean>>() {
+                    override fun onSuccess(response: List<ChaptersBean>) {
+                        it.onSuccess(response)
+                    }
+                })
+        }
     }
-
 }
